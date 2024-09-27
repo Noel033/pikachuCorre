2719aa580dac275a13fa87cfbc2e16878511f921
Antibush <noeljefry71@gmail.com>
2024-09-27 12:50:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,64 @@
 const pokemon = document.getElementById("pokemon");
-
-let posX = 200;
-let posY = 200;
+let posX = 0;
+let posY = 0;
 const step = 10;
 
-document.addEventListener("keydown", (e) => {
+function updatePosition() {
+  pokemon.style.transform = `translate(${posX}px, ${posY}px)`;
+}
+
+function handleKeyDown(e) {
   switch (e.key) {
     case "ArrowUp":
       if (posY > 0) posY -= step;
       break;
     case "ArrowDown":
-      if (posY < gameArea.clientHeight - pokemon.clientHeight) posY += step;
+      if (posY < window.innerHeight - 50) posY += step;
       break;
     case "ArrowLeft":
       if (posX > 0) posX -= step;
       break;
     case "ArrowRight":
-      if (posX < gameArea.clientWidth - pokemon.clientWidth) posX += step;
+      if (posX < window.innerWidth - 50) posX += step;
       break;
   }
   updatePosition();
-});
-
-function updatePosition() {
-  pokemon.style.left = posX + "px";
-  pokemon.style.top = posY + "px";
 }
 
-updatePosition();
+document.addEventListener("keydown", handleKeyDown);
+
+// Soporte para pantallas táctiles
+let touchStartX, touchStartY;
+
+document.addEventListener("touchstart", (e) => {
+  touchStartX = e.touches[0].clientX;
+  touchStartY = e.touches[0].clientY;
+});
+
+document.addEventListener("touchmove", (e) => {
+  e.preventDefault(); // Previene el scrolling
+  const touchEndX = e.touches[0].clientX;
+  const touchEndY = e.touches[0].clientY;
+  const dx = touchEndX - touchStartX;
+  const dy = touchEndY - touchStartY;
+
+  if (Math.abs(dx) > Math.abs(dy)) {
+    // Movimiento horizontal
+    if (dx > 0 && posX < window.innerWidth - 50) {
+      posX += step;
+    } else if (dx < 0 && posX > 0) {
+      posX -= step;
+    }
+  } else {
+    // Movimiento vertical
+    if (dy > 0 && posY < window.innerHeight - 50) {
+      posY += step;
+    } else if (dy < 0 && posY > 0) {
+      posY -= step;
+    }
+  }
+
+  updatePosition();
+  touchStartX = touchEndX;
+  touchStartY = touchEndY;
+});
